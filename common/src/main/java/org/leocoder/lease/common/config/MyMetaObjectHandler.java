44e89025efd4bf3dbef93467f6b200327965e589
gaoziman <2942894660@qq.com>
2024-09-07 19:52:07 +0800

    feat/添加mybatisplus自定义填充器

---
+++ common/src/main/java/org/leocoder/lease/common/config/MyMetaObjectHandler.java
@@ -0,0 +1,33 @@
+package org.leocoder.lease.common.config;
+
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.ibatis.reflection.MetaObject;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+/**
+ * @author : Leo
+ * @version 1.0
+ * @date 2024-08-27 23:40
+ * @description : mybatisplus自定义填充器
+ */
+@Slf4j
+@Component
+public class MyMetaObjectHandler implements MetaObjectHandler {
+
+    @Override
+    public void insertFill(MetaObject metaObject) {
+        log.info("开始插入填充...");
+        this.strictInsertFill(metaObject, "createTime", Date::new, Date.class);
+        this.strictInsertFill(metaObject, "updateTime", Date::new, Date.class);
+        this.strictInsertFill(metaObject, "delFlag",   Integer.class,  0);
+    }
+
+    @Override
+    public void updateFill(MetaObject metaObject) {
+        log.info("开始更新填充...");
+        this.strictUpdateFill(metaObject, "updateTime",Date::new, Date.class);
+    }
+}
